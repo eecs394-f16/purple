38d7e5e5dae7a2e1d7601791d8f6362ae2bd749c
mazhang2718 <mazhang2718@gmail.com>
2016-09-25 13:46:54 -0500

    Testing geolocation retrieval

--- app/example/index.js
+++ app/example/index.js
@@ -2,3 +2,13 @@ angular.module('example', [
   // Declare here all AngularJS dependencies that are shared by the example module.
   'supersonic'
 ]);
+
+
+angular
+  .module('geolocation')
+  .controller('IndexController', function($scope, supersonic) {
+    // Controller functionality here
+    $scope.getPosition = function() {
+      supersonic.ui.dialog.alert("Interstellar!");
+    };
+  });

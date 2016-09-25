0c6a97d848916334bdff7d85773bb74f8319e225
James Willis <jameswillis2017@u.northwestern.edu>
2016-09-25 14:53:15 -0500

    displays text

---
+++ app/Feed/scripts/IndexController.js
@@ -0,0 +1,7 @@
+angular
+  .module('Feed')
+  .controller('IndexController', function($scope, supersonic) {
+    // Controller functionality here
+	$scope.name = 'tech';
+  $scope.contents = 'this is a string';
+  });

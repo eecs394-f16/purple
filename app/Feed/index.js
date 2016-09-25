850f4fa9b1373e8dc79fa8eb12dc4d9bc1956d5c
fahadkh <fahadkhan2017@u.northwestern.edu>
2016-09-25 17:03:35 -0500

    filling in data from backend

--- app/Feed/index.js
+++ app/Feed/index.js
@@ -3,20 +3,3 @@ angular
   	'common']
 
   );
-
-window.onload = function() {
-  var content = ['abc', '123', 'strings galore']
-
-  var makeDIVS = function(array) {
-    var list = document.createElement('div');
-
-    for (var i = 0; i < array.length; i++) {
-      var item = document.createElement('div');
-      // item.classname = 'container-fluid';
-      item.appendChild(document.createTextNode(array[i]));
-      list.appendChild(item);
-    }
-    return list;
-  }
-  document.appendElementById('feed').appendChild(makeDIVS(content));
-}
--- app/Feed/scripts/IndexController.js
+++ app/Feed/scripts/IndexController.js
@@ -1,11 +1,15 @@
 angular
   .module('Feed')
-  .controller('IndexController', function($scope, supersonic) {
+  .controller('IndexController', function($scope, $interval, supersonic) {
     // Controller functionality here
   	$scope.curr_loc = 'No nearby wall';
     $scope.contents = undefined;
-    $scope.getContent = function() {
-      $scope.curr_loc = 'tech';
-      $scope.contents = ['123', 'abc', 'last'];
-  };
+
+    var getContent = function() {
+      $scope.curr_loc = 'Tech';
+      $scope.contents = ['Memory 1', 'Memory 2', 'Memory 3', 'Memory 4'];
+  	};
+  	getContent();
+
+  	$interval(getContent, 5000);
 });

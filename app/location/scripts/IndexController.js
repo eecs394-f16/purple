ab3b20bb39f877e5a1b939e44a1c17b3b1590c97
lennyKim <heliotherapyy@gmail.com>
2016-09-26 11:52:09 -0500

    (add) add firebase to geolocation module

--- app/location/scripts/IndexController.js
+++ app/location/scripts/IndexController.js
@@ -1,35 +1,6 @@
-var firebase = require('firebase');
-
 angular
   .module('location')
   .controller("IndexController", function ($scope, Location, supersonic) {
-
-    // Firebase Setting
-    var config = {
-          apiKey: "AIzaSyDAuhBy07kgbtxrkWjHu76bS7-Rvsr2Oo8",
-          authDomain: "purple-b06c8.firebaseapp.com",
-          databaseURL: "https://purple-b06c8.firebaseio.com",
-          storageBucket: "purple-b06c8.appspot.com",
-          messagingSenderId: "396973912921"
-        };
-
-    firebase.initializeApp(config);
-    var database = firebase.database();
-
-    function saveData(name, long, lat) {
-      firebase.database().ref('/location/' + name).set({
-        longitude: long,
-        langitude: lat
-      })
-    }
-
-    var locations;
-
-    firebase.database().ref('/location').once('value').then(function(snapshot) {
-      locations = snapshot.val();
-    });
-
-
 
     /* Supersonic */
     $scope.locations = null;
